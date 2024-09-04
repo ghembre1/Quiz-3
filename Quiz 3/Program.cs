be2a00eb6f7a39fb0f572aabc6eee22dfba64f49
Gary Hembree <gbhembree22@amarillocollege.com>
2024-09-04 12:59:44 -0500

    Add project files.

---
+++ Quiz 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompts user to input a string
+            Console.WriteLine("Enter a string");
+            
+            // Makes user input a variable
+            string stringA = Console.ReadLine();
+
+           
+
+            bool is Palindrome = true;
+            for (int i = 0; i<stringA.Lenght; i++)
+            {
+                if (stringA[i] == stringA[stringA.lenght - 1])
+                {
+                    ispalindrome = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine
+
+        }
+    }
+}
